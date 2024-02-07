a4cebc7150867cda1d4e523b5a2ec4b00b376716
azsorlex <arozsa@protonmail.com>
2024-02-07 16:48:11 +1100

    Further optimise performance

--- Game of Life/Game Window.cs
+++ Game of Life/Game Window.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 namespace Game_of_Life
 {
     public partial class GameWindow : Form
@@ -18,7 +20,7 @@ namespace Game_of_Life
             Generate_Game_Matrix();
         }
 
-        private IEnumerable<Panel> Live_Or_Die(Panel panel, bool firstInvocation = true)
+        private IEnumerable<Panel> Live_Or_Die(Panel panel, ConcurrentDictionary<string, Panel>? existingInvocations, bool firstInvocation = true)
         {
             var name = panel.Name.Split("_");
             var y = int.Parse(name[0]);
@@ -35,15 +37,15 @@ namespace Game_of_Life
                         if (neighbour.BackColor == Color.Yellow)
                             activeNeighbours++;
 
-                        if (firstInvocation && Live_Or_Die(neighbour, false).Any())
+                        if (firstInvocation && !(existingInvocations?.TryGetValue(neighbour.Name, out _) ?? false) && Live_Or_Die(neighbour, null, false).Any())
                             yield return neighbour;
                     }
                 }
             }
 
-            if ((panel.BackColor == Color.Gray && activeNeighbours == 3) ||
-                (panel.BackColor == Color.Yellow && activeNeighbours < 2) ||
-                (panel.BackColor == Color.Yellow && activeNeighbours >= 4))
+            if ((activeNeighbours == 3 && panel.BackColor == Color.Gray) ||
+                (activeNeighbours < 2 && panel.BackColor == Color.Yellow) ||
+                (activeNeighbours >= 4 && panel.BackColor == Color.Yellow))
                 yield return panel;
         }
 
@@ -51,16 +53,20 @@ namespace Game_of_Life
 
         private void playPauseButton_Click(object sender, EventArgs e) => ((Button)sender).Text = (refreshTimer.Enabled = !refreshTimer.Enabled) ? "Pause" : "Play";
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void refreshTimer_Tick(object sender, EventArgs e)
         {
-            var invocations = new List<Panel>();
+            var invocations = new ConcurrentDictionary<string, Panel>();
 
             // Get the panels that need to be updated without changing their states
-            foreach (var panel in panelMatrix.SelectMany(row => row.Where(p => p.BackColor == Color.Yellow)))
-                invocations.AddRange(Live_Or_Die(panel).Where(x => !invocations.Contains(x)));
+            Parallel.ForEach(panelMatrix.SelectMany(row => row.Where(p => p.BackColor == Color.Yellow)), panel =>
+            {
+                foreach (var x in Live_Or_Die(panel, invocations))
+                    invocations.TryAdd(x.Name, x);
+            });
 
             // Update the states of the returned panels
-            invocations.ForEach(panel => panel.Toggle_Colour());
+            foreach (var panel in invocations.Values)
+                panel.Toggle_Colour();
         }
     }
 }
